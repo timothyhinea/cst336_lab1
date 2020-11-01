9029ddcbe384e4350bf1ec252ce979441d38e108
EC2 Default User <ec2-user@ip-172-31-1-191.us-west-1.compute.internal>
2020-11-01 04:11:10 +0000

    adding files to trick heroku

--- index.php
+++ index.php
@@ -1,9 +1,1 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title> </title>
-    </head>
-    <body>
-        <h1>Welcome To my website!</h1>
-    </body>
-</html>
+<?php include_once("index.html"); ?>
